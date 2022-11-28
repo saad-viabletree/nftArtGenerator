b2c9705684224ec24ef0a1a56538b3f857770d34
msaadalam <64380190+msaadalam@users.noreply.github.com>
2022-11-28 20:14:37 +0500

    new nft set generated

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const saveLayer = (_canvas,_edition) => {
    fs.writeFileSync(`./output/${_edition}.png`,_canvas.toBuffer("image/png"));
   // console.log("image created")
 }
+attributes = [];
 const addMetadata = (_edition) => {
     let dateTime = Date.now();
     let tempMetadata = {
@@ -22,15 +23,21 @@ const addMetadata = (_edition) => {
       decodedHash:decodedHash,
       edition:_edition,
       date:dateTime,
+      image:`ipfs://QmVwc4V7PD1LCWntoBiv7SHtgWqGFubWTH5eBPucJ3DcY5/${_edition}.png`,
       attributes: attributes
     }
-   metadata.push(tempMetadata);
-   attributes = [];
-   hash = [];
-   decodedHash = [];
+
+   fs.writeFileSync(`./output/metadata/${_edition}.json`, JSON.stringify(tempMetadata))
+//    attributes = [];
+  // attributes.length = 0;
+  
+ //  metadata.push(tempMetadata);
+   
+ //  hash = [];
+ //  decodedHash = [];
 };
 
-const addAttributes = (_element,_layer) => {
+const  addAttributes = (_element,_layer) => {
     let tempAttr = {
         id:_element.id,
         layer:_layer.name,
@@ -38,8 +45,6 @@ const addAttributes = (_element,_layer) => {
         rarity:_element.rarity
     }
     attributes.push(tempAttr)
-    hash.push(_layer.id)
-    hash.push(_element.id)
     decodedHash.push({[_layer.id]:_element.id})
 
 }
@@ -48,25 +53,18 @@ const drawLayer = async (_layer,_edition) => {
     const element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)]
     addAttributes(element,_layer)
     const image = await loadImage(`${_layer.location}${element.filename}`);
-  //  console.log(_layer.location)
-  //  console.log(element.filename)
-  //  console.log(image)
     ctx.drawImage(image,_layer.position.x,_layer.position.y,_layer.size.width,_layer.size.height);
- //   console.log(`I created ${_layer.name} layer, and choose element ${element.name}`)
-    saveLayer(canvas,_edition)
+   const re =  saveLayer(canvas,_edition)
 }
 
 for(let i=1; i <= edition; i++){
     layers.forEach((layer) => {
-     //   console.log("layers: ",layer)
-    
         drawLayer(layer, i);
         addMetadata(i)
-        //console.log(layer)
+        
     });
-}
+    attributes = [];
+    decodedHash = [];
+    hash = [];
 
-fs.readFile("./output/_metadata.json",(err,data)=>{
-    if(err) throw err;
-    fs.writeFileSync("./output/_metadata.json", JSON.stringify(metadata))
-})
+}
